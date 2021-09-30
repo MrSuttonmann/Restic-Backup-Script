982e3681a73809b4d30fc02734f019c1dc91a073
MrSuttonmann <7480500+MrSuttonmann@users.noreply.github.com>
2021-09-30 13:14:18 +0100

    Fix typo

--- backup.sh
+++ backup.sh
@@ -20,7 +20,7 @@ echo "$(timestamp): restic.sh started" | tee -a $LOG_FILE
 restic -r $REPOSITORY backup $BACKUP_PATH
 restic -r $REPOSITORY forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --keep-yearly 7 | tee -a $LOG_FILE
 restic -r $REPOSITORY prune
-restic -r $RESPOSITORY check | tee -a $LOG_FILE
+restic -r $REPOSITORY check | tee -a $LOG_FILE
 
 # insert timestamp into log
 printf "\n\n"
